3cae38d33dfe9bae42046bd80f0af475a93ed97d
darmhoo <darmhoo@hotmail.com>
2020-02-04 17:59:34 +0100

    todo list api created

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -2,9 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Todo;
 use Illuminate\Http\Request;
 
 class TodoController extends Controller
 {
     //
+    public function index(){
+        $todos = Todo::all();
+        return response()->json(
+            [
+               'status' => 'success',
+                'todos' => $todos
+            ]
+        );
+    }
+
 }
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -16,3 +17,4 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::get('/todos', 'TodoController@index');
