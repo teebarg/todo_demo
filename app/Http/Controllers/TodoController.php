e4a8637f5b0aebc5aa58719d0e61e33f80849e28
Beaft <teebarg01@gmail.com>
2020-02-04 18:05:44 +0100

    add todo

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -2,9 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Todo;
 use Illuminate\Http\Request;
 
 class TodoController extends Controller
 {
-    //
+    public function store(){
+        $this->validate(request(), [
+            'todo'=>'required|min:1',
+            'status' => 'required|boolean'
+        ]);
+       $result =  Todo::create(\request(['todo', 'status']));
+       return response()->json([$result], 200);
+    }
 }
