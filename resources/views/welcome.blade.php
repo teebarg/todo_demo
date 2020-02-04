3ef878035b46a159231540202f6079b67612477a
Beaft <teebarg01@gmail.com>
2020-02-04 16:35:48 +0100

    rename to Todo

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Todo App
                 </div>
 
                 <div class="links">
